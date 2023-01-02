9e5ab8eae77fabcf3440001d96f19729ac80eccb
Bruno Breña Ramírez <brcalix@gmail.com>
2023-01-02 10:01:41 -0500

    styles: images

--- src/constants/enviroment.ts
+++ src/constants/enviroment.ts
@@ -1,6 +1,2 @@
-//* Development
-// export const backendUrl = import.meta.env.VITE_BACKEND_URL;
-// export const whatsappNumber = import.meta.env.VITE_WHATSAPP_NUMBER;
-//* Production
-export const backendUrl = 'https://backend.almacen.jambelectronics.com/';
-export const whatsappNumber = 51923563714;
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const whatsappNumber = import.meta.env.VITE_WHATSAPP_NUMBER;
--- src/features/header/components/title.component.tsx
+++ src/features/header/components/title.component.tsx
@@ -1,9 +1,10 @@
-import { PageContent } from "../../../constants/page_content";
+// import { PageContent } from "../../../constants/page_content";
 
 function Title() {
   return (
     <div className="text font-bold text-2xl px-2 lg:px-4 lg:border-r-2 lg:border-r-black">
-      {PageContent.title}
+      {/* {PageContent.title} */}
+      <img src="/logo.png" alt="logo" className="h-8 inline-block" />
     </div>
   );
 }
